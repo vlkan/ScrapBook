fb8c77a90bd60c9c4e6bb1b456d4098b496b38b5
vlkan <thkvolkanonder@gmail.com>
2025-03-21 14:31:03 +0300

    ref key review

--- RefOutParams/Program.cs
+++ RefOutParams/Program.cs
@@ -2,6 +2,17 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int value = 5;
+        Console.WriteLine("Main: Before Call Method: {0}", value);
+        ChangeValue(ref value);
+        Console.WriteLine("Main: After Call Method: {0}", value);
+    }
+
+    public static void ChangeValue(ref int value)
+    {
+        Console.WriteLine("ChangeValue: Before Changing Value: {0}", value);
+        value = 10;
+        Console.WriteLine("ChangeValue: After Changing Value: {0}", value);
+
     }
 }
