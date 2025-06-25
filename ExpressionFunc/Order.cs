92aa714121ad755d4841c0bf5d9aed09fda2e55c
Volkan Önder <volkan.onder@csb.gov.tr>
2025-06-25 17:33:31 +0300

    ExpressionFunc usage

---
+++ ExpressionFunc/Order.cs
@@ -0,0 +1,9 @@
+﻿namespace ExpressionFunc;
+
+public class Order
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public int Amount { get; set; }
+    public DateTime ReleaseDate { get; set; }
+}
---
+++ ExpressionFunc/Program.cs
@@ -0,0 +1,36 @@
+﻿using ExpressionFunc;
+using System.Linq.Expressions;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        IEnumerable<Order> orders = new List<Order>
+        {
+            new Order
+            {
+                Id = Guid.NewGuid(),
+                Name = "Laptop",
+                Amount = 2,
+                ReleaseDate = new DateTime(2025, 6, 1)
+            },
+            new Order
+            {
+                Id = Guid.NewGuid(),
+                Name = "Mouse",
+                Amount = 10,
+                ReleaseDate = new DateTime(2025, 6, 15)
+            },
+            new Order
+            {
+                Id = Guid.NewGuid(),
+                Name = "Keyboard",
+                Amount = 5,
+                ReleaseDate = new DateTime(2025, 5, 20)
+            }
+        };
+        Expression<Func<Order, bool>> filter = o => o.Amount > 100 && o.Name == "Paid";
+
+        orders.Where(filter.Compile());
+    }
+}
