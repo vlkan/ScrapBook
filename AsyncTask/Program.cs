d33d5866cba476e16b1e60caa47d7f01c26bbab1
vlkan <thkvolkanonder@gmail.com>
2025-08-03 23:27:45 +0300

    async tast denemelerim.

---
+++ AsyncTask/Program.cs
@@ -0,0 +1,30 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Example_1();
+    }
+
+    static void Example_1()
+    {
+        List<Task> tasks = new();
+        AsyncLocal<int> local = new();
+
+        for (int i = 0; i < 10; i++)
+        {
+            local.Value = i;
+            var ii = i;
+
+            var t = Task.Run(() =>
+            {
+                Console.WriteLine("Value: {0}", local.Value);
+                Task.Delay(1000).Wait();
+            });
+
+            tasks.Add(t);
+        }
+
+        Task.WhenAll(tasks).Wait();
+    }
+}
+
